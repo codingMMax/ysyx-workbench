fde53ce6bf5b047c81116dab1ce56dc7b0449a06
Max_gs66_ubuntu <Max_gs66_ubuntu@max>
2022-07-12 11:39:52 -0700

    add more defines

--- npc/cpu/defines.v
+++ npc/cpu/defines.v
@@ -6,16 +6,17 @@
 `define Imm_20     19:0
 
 //define read bit-length
-`define read_no   2'00 // no load operation required
-`define read_byte 2'01 // load 8-bit data from memroy to reg
-`define read_hw   2'10 // load 16-bit half-word data from memory to reg
-`define read_fw   2'11 // load 32-bit full-word data from memory to reg
+`define read_no   2'b00 // no load operation required
+`define read_byte 2'b01 // load 8-bit data from memroy to reg
+`define read_hw   2'b10 // load 16-bit half-word data from memory to reg
+`define read_fw   2'b11 // load 32-bit full-word data from memory to reg
 
 //define shift operation
-`define no_shift 3'b000
-`define sign_shift 3'b001
-`define unsign_shift 3'b010
-`define zero_fill 3'b011
+`define no_shift 3'b000   // no immediate number
+`define sign_shift 3'b001 // signed shift to make it 64-bit width
+`define unsign_shift 3'b010 // unsigned shift to make it 64-bit width
+`define zero_fill 3'b011 // place the 20-bit instruction in the first 20-bit  
+                        // and zero fill the rest bits
 
 //define ALU control signal
 `define ALU_NO   4'b0000
@@ -24,9 +25,8 @@
 `define ALU_AND  4'b0011
 `define ALU_OR   4'b0100
 `define ALU_XOR  4'b0101
-`define ALU_RAS  4'b0110 // right arithematci shift
-`define ALU_RLS  4'b0111 // right logic shift
-`define ALU_LAS  4'b1000 // left arithemtic shift
-`define ALU_LLS  4'b1001 // left logic shift
-`define ALU_SCOMP 4'b1010 // compare the input 2 data as signed digits
-`define ALU_UCOMP 4'b1011 // compare the input 2 data as unsigned digits
+`define ALU_SAR  4'b0110 // right arithematci shift
+`define ALU_SLR  4'b0111 // right logic shift
+`define ALU_SLL  4'b1000 // left logic shift
+`define ALU_SCOMP 4'b1001 // compare the input 2 data as signed digits
+`define ALU_UCOMP 4'b1010 // compare the input 2 data as unsigned digits
